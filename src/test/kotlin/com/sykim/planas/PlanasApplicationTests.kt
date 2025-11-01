d91e58fb4c20cc2cc0ddb6606bf30c0c66980048
sangyoon kim <sangyun0904@gmail.com>
2025-11-01 11:39:47 +0900

    프로젝트 생성

---
+++ src/test/kotlin/com/sykim/planas/PlanasApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sykim.planas
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PlanasApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
